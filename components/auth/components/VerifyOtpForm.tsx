9f63975409fa8d580b39beeaf9a15fb49f256dac
iSyl.js <isylvestersam@gmail.com>
2026-03-23 23:11:49 +0100

    setup zod resolver and hook form for otp validation

--- components/auth/components/VerifyOtpForm.tsx
+++ components/auth/components/VerifyOtpForm.tsx
@@ -3,14 +3,44 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { loginSchema, LoginSchema, signupSchema, SignupSchema } from "@/schemas/AuthSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { VerfiyOtpSchema, verifiyOtpSchema } from "@/schemas/VerifyOtpSchema";
 
 
 export default function VerifyOtpForm() {
+  const [isLoading, setIsLoading] = useState(true);
+  // const router = useRouter();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<VerfiyOtpSchema>({
+    resolver: zodResolver(verifiyOtpSchema),
+    defaultValues: {
+      token: ''
+    }
+  })
+
+  function submitFunc(data: any) {
+    console.log(data);
+
+  }
+
   return (
-    <form >
-      <Input className="text-center text-lg tracking-[0.75em] py-5 w-full text-bold lg:py-6" type="text" inputMode="numeric" pattern="\d{6}" maxLength={6} placeholder="******" />
-      <Button className="w-full mt-2 py-5 rounded-md md:mt-4 flex items-center " >
-        <p className="uppercase font-bold text-xs">Verify & Continue</p>
+    <form onSubmit={handleSubmit(submitFunc)} className="flex flex-col" >
+      {
+        errors.token && (
+          <p className="text-destructive text-xs text-left mr-auto mb-2 italic">{errors.token.message}</p>
+        )
+      }
+      <Input
+        aria-invalid={!!errors.token}
+        {...register('token')}
+        className="text-center text-lg tracking-[0.75em] py-5 w-full text-bold lg:py-6" type="text" inputMode="numeric" pattern="\d{6}" maxLength={6} placeholder="******" />
+
+      <Button className="w-full mt-3 py-5 rounded-md md:mt-3 flex items-center justify-center " >
+        <p className="uppercase font-bold text-xs">verify & continue</p>
         <ArrowRight />
       </Button>
     </form>
--- schemas/VerifyOtpSchema.ts
+++ schemas/VerifyOtpSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const verifiyOtpSchema = z.object({
-  token: z.string(),
+  token: z.string('Invalid OTP').nonempty('Please enter your OTP')
 });
 
 export type VerfiyOtpSchema = z.infer<typeof verifiyOtpSchema>;
