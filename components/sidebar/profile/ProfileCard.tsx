7d61a115c9f29fd0106a9b47a77708764fe7a7d7
iSyl.js <isylvestersam@gmail.com>
2026-03-26 22:32:14 +0100

    usercard 85%

--- components/sidebar/profile/ProfileCard.tsx
+++ components/sidebar/profile/ProfileCard.tsx
@@ -1,4 +1,4 @@
-import { EllipsisVertical } from "lucide-react";
+import { EllipsisVertical, LogOut } from "lucide-react";
 import UserAvatar from "../../shared/UserAvatar";
 import { Button, buttonVariants } from "../../ui/button";
 import { Card } from "../../ui/card";
@@ -6,6 +6,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { profileCardData } from "./profileCardData";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 
 export default function ProfileCard() {
@@ -24,25 +25,30 @@ export default function ProfileCard() {
       </Card>
 
       <PopoverContent >
-        <div className="flex flex-col items-center px-4">
+        <div className="flex flex-col items-center px-2 ">
           <div className="text-center flex flex-col items-center">
             <UserAvatar className="w-14 h-14 border-2 border-primary mb-2" />
             <h3 className="text-foreground text-[16px]">Alex Rivera</h3>
             <p className="uppercase tracking-wider text-xs">Beta User</p>
           </div>
 
-          <div>
+          <div className="w-full mt-3 flex flex-col">
             {
               profileCardData.map(data => (
-                <Link key={data.title} href={data.to} className={cn(buttonVariants({ variant: 'ghost' }), 'w-full ')} >
+                <Link key={data.title} href={data.to} className={cn(buttonVariants({ variant: 'ghost', size: 'lg' }), 'w-full flex justify-start')} >
                   {(() => {
                     const Icon = data.icon;
-                    return <Icon className="mr-2 h-4 w-4" />;
+                    return <Icon className="mr-1 h-4 w-4" />;
                   })()}
                   {data.title}
                 </Link>
               ))
             }
+            <Separator className="mb-3" />
+            <Button variant={'destructive'} size={'lg'} className="w-full flex justify-start py-3">
+              <LogOut />
+              <p>Logout</p>
+            </Button>
           </div>
         </div>
       </PopoverContent>
