406f2d6491012ed567c5b92a80563aaba63f23e4
iSyl.js <isylvestersam@gmail.com>
2026-03-27 19:40:41 +0100

    write user pfp upload logic

--- components/settings/IdentityCard.tsx
+++ components/settings/IdentityCard.tsx
@@ -5,20 +5,40 @@ import UserAvatar from "../shared/UserAvatar";
 import { Card, CardHeader } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 
 export default function IdentityCard() {
   const [imageUrl, setImageUrl] = useState('/images/pfp.png')
+
+  function handleChangePfp(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+
+    if (file) {
+      const previewUrl = URL.createObjectURL(file)
+      setImageUrl(previewUrl)
+
+      return () => URL.revokeObjectURL(previewUrl)
+    }
+  }
 
 
 
   return (
     <Card>
       <CardHeader>
-        <Label className="w-fit hover:cursor-pointer rounded-full overflow-hidden shadow-lg border-3 border-card-foreground/50" >
-          <Image src={imageUrl} height={120} width={120} alt='User Profile' />
-          <Input type="file" hidden />
+        <Label
+          className="w-28 h-28 object-cover hover:cursor-pointer rounded-full overflow-hidden shadow-lg border-3 border-card-foreground/50" >
+          <Image
+            src={imageUrl}
+            height={120}
+            width={120}
+            alt='User Profile' />
+          <Input
+            onChange={handleChangePfp}
+            type="file"
+            hidden
+            accept="image*" />
         </Label>
       </CardHeader>
     </Card>
