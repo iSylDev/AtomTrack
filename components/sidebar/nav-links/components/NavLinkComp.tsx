388d4bf35d3c264b17e2b6256a41d46d267bf27c
iSyl.js <isylvestersam@gmail.com>
2026-03-30 23:23:17 +0100

    implement active navlink classes

--- components/sidebar/nav-links/components/NavLinkComp.tsx
+++ components/sidebar/nav-links/components/NavLinkComp.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils';
 import * as Icons from 'lucide-react'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -16,9 +17,12 @@ export default function NavLinkComponent({ linkTo, title, icon, subLinks }: prop
   const pathname = usePathname()
   const Icon = icon ? (Icons[icon] as Icons.LucideIcon) : null;
 
+  const isActive = pathname.includes(linkTo)
+
   return (
     <div className="text-[14px]">
-      <Link href={linkTo} className="flex gap-2 items-center text-card-foreground hover:text-chart-1 transition-all ease-in-out duration-300 hover:bg-chart-1/10 py-2 px-3 rounded-lg">
+      <Link href={linkTo} className={cn('flex gap-2 items-center py-2 px-3 rounded-lg',
+        isActive ? 'bg-chart-1/20 text-chart-1' : 'text-card-foreground hover:text-chart-1 transition-all ease-in-out duration-300 hover:bg-chart-1/10')} >
         {Icon && (<Icon className="w-[20px]" />)}
         <p>{title}</p>
       </Link>
@@ -27,7 +31,9 @@ export default function NavLinkComponent({ linkTo, title, icon, subLinks }: prop
         {
           subLinks && (
             subLinks.map(link => (
-              <Link href={link.to} key={link.title} className="mx-2  mb-1 text-sm  flex gap-2 items-center text-card-foreground hover:text-chart-2 transition-all ease-in-out duration-300 hover:bg-chart-2/10 py-2 px-3 rounded-lg">
+              <Link href={link.to} key={link.title} className={cn("mx-2  mt-1 mb-1 text-sm  flex gap-2 items-center py-2 px-3 rounded-lg",
+                pathname.includes(link.to) ? 'bg-chart-2/20 text-chart-2' : 'text-card-foreground hover:text-chart-2 transition-all ease-in-out duration-300 hover:bg-chart-2/10'
+              )}>
                 <p>{link.title}</p>
               </Link>
             ))
