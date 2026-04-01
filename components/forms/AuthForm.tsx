85641e220b631928f74e7b94aac5e6fd8d6348dc
iSyl.js <isylvestersam@gmail.com>
2026-04-01 20:24:39 +0100

    initiate auth refactoring

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -23,17 +23,9 @@ export default function AuthForm() {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
-  const searchParams = useSearchParams();
-  const mode = searchParams.get("mode") || 'sign-up'
-  const isLogin = mode === 'sign-in'
-  console.log(mode);
-
-  const currentSchema = isLogin ? loginSchema : signupSchema
-
-  const { register, handleSubmit, formState: { errors } } = useForm<LoginSchema | SignupSchema>({
-    resolver: zodResolver(currentSchema),
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
-      username: '',
       email: '',
     },
   })
@@ -63,24 +55,12 @@ export default function AuthForm() {
     <div className="w-full">
       <Card className="w-full max-w-md mx-auto px-2 pb-7 lg:mt-7">
         <CardHeader className="mb-5">
-          <CardTitle className='text-2xl font-bold text-foreground'>{isLogin ? 'Welcome Back' : 'Create your Account'}</CardTitle>
-          <CardDescription className='text-sm'>{isLogin ? 'Sign in to continue to your account.' : 'Join the ecosystem of high-performance tracking.'}</CardDescription>
+          <CardTitle className='text-2xl font-bold text-foreground'>Continue to Dashboard</CardTitle>
+          <CardDescription className='text-sm'>Step into the ecosystem of atomic tracking.</CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit(submitFunc)}>
             <div className="grid gap-5">
-              {
-                !isLogin && (
-                  <div className="grid gap-3">
-                    {/* <Label htmlFor="username">Username</Label> */}
-                    <div className="relative">
-                      <User className="absolute left-3.5 top-1/2 -translate-y-1/2 stroke-card-foreground/50" size={17} />
-                      <Input id="username" type="text" placeholder="Username" className="pl-10 py-5 placeholder:text-card-foreground/30 rounded-sm placeholder:text-sm" {...register('username')} />
-                    </div>
-                    {errors && 'username' in errors && errors.username && <p className="text-destructive text-xs italic -mt-2">{errors.username.message}!</p>}
-                  </div>
-                )
-              }
               <div className="grid gap-3">
                 {/* <Label htmlFor="email">Email</Label> */}
                 <div className="relative">
@@ -92,16 +72,13 @@ export default function AuthForm() {
             </div>
             <Button className="w-full mt-4 py-5 rounded-md md:mt-5" >
               {
-                !isLoading && (<p>{!isLogin ? 'Get Started' : 'Continue'}</p>)
+                !isLoading && (<p>Continue</p>)
               }{
                 isLoading && (<Loader2 className="animate-spin" />)
               }
             </Button>
           </form>
 
-          <p className="text-center mt-7 text-sm ">{isLogin ? 'New to AtomTrack?' : 'Already have an account?'}
-            <Link href={isLogin ? `/auth?mode` : `/auth?mode=sign-in`} className="text-primary font-semibold hover:underline transition-all duration-300 ease-in-out pl-1">{!isLogin ? 'Sign in' : 'Sign up'}</Link>
-          </p>
 
           <div>
             <div className="flex justify-center items-center mt-4 gap-3">
@@ -113,11 +90,11 @@ export default function AuthForm() {
             <div className="flex flex-col gap-2 mt-2">
               <Button className="w-full py-5 text-sm" variant='outline'>
                 <Image src='/images/auth-icons/google-icon.png' alt="Google logo" width={17} height={15} />
-                {isLogin ? 'Continue with Google' : 'Sign in with Google'}
+                <p>Continue with Google</p>
               </Button>
               <Button className="w-full py-5 text-sm" variant={'outline'}>
                 <Image src='/images/auth-icons/github-icon.png' alt="Google logo" width={25} height={25} />
-                {isLogin ? 'Continue with Github' : 'Sign in with Github'}
+                <p>Continue with Discord</p>
               </Button>
             </div>
           </div>
