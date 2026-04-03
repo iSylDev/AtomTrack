a474e9f6ac1c82041223cd254fcd2a6fd6202933
iSyl.js <isylvestersam@gmail.com>
2026-04-03 01:03:45 +0100

    create getRandomPfp

--- actions/auth-actions/createUserAction.ts
+++ actions/auth-actions/createUserAction.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import getRandomPfp from "@/helpers/pfpSelector";
 import { createClient } from "@/utils/supabase/server";
 
 export async function createUser() {
+  const randomPfp = getRandomPfp();
   const supabase = await createClient();
 
   // Get the authenticated user from the session
@@ -10,6 +12,8 @@ export async function createUser() {
     data: { user },
     error: getUserError,
   } = await supabase.auth.getUser();
+  console.log(user);
+  
 
   if (!user || getUserError) {
     return { success: false, message: "Error fetching user details." };
@@ -20,31 +24,27 @@ export async function createUser() {
     .from("users")
     .select("user_id")
     .eq("user_id", user.id)
-    .single();
+    .maybeSingle();
 
     // return here if profile already exists
   if (exisitingProfile) {
     return { success: true, message: "Profile already exists" };
   }
 
-  //  Upsert the profile if its a new user
-  const { error: upsertError } = await supabase.from("users").upsert(
-    {
-      user_id: user.id,
-      email: user.email,
-      username:
-        user.user_metadata?.username || user.email?.split("@")[0] || "New User",
-      profile_picture: "",
-      theme: "dark",
-      task_deadline_notif: true,
-      weekly_reports_notif: true,
-      marketing_updates_notif: true,
-    },
-    { onConflict: "user_id" },
-  );
+  //  Inset the profile if its a new user
+  const { error: insertError } = await supabase.from("users").insert({
+    user_id: user.id,
+    email: user.email,
+    username: user.user_metadata?.username || user.email?.split("@")[0] || "New User",
+    profile_picture: randomPfp,
+    theme: "dark",
+    task_deadline_notif: true,
+    weekly_reports_notif: true,
+    marketing_updates_notif: true,
+  });
 
-  if (upsertError) {
-    console.error("Upsert Error:", upsertError);
+  if (insertError) {
+    console.error("Insert Error:", insertError);
     return {
       success: false,
       message: "Error Creating user profile. Please try again.",
