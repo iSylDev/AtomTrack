90be96acb1cb591055f825df1681ca7e5b4adeea
iSyl.js <isylvestersam@gmail.com>
2026-04-05 00:26:46 +0100

    refactor useUser

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,8 +1,8 @@
 "use client";
 
+import assert from "@/utils/assertions";
 import { createClientInBroswer } from "@/utils/supabase/client";
 import { useQuery } from "@tanstack/react-query";
-import { toast } from "sonner";
 
 const useUser = () => {
   const supabase = createClientInBroswer();
@@ -10,66 +10,28 @@ const useUser = () => {
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["user"],
     queryFn: async () => {
+      // Get the currently logged in user
       const {
         data: { user: authUser },
         error: authError,
       } = await supabase.auth.getUser();
 
-      if (authError || !authUser) throw new Error("Not authenticated");
+      assert(!authError && authUser, "Not Authenticated");
 
+      // Get the user's data from our supabase table
       const { data: userData, error: userTableError } = await supabase
         .from("users")
         .select("*")
         .eq("user_id", authUser.id)
         .single();
 
-      if (authError || !authUser) throw new Error("Not authenticated");
+      assert(!userTableError && userData, "Profile not found");
 
       return userData;
     },
     retry: 2,
+    enabled: typeof window !== "undefined",
   });
   return { user: data, isLoading, error, refetch };
 };
 export { useUser };
-
-// const useFetchUser = () => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState("");
-
-//   const supabase = createClientInBroswer();
-
-//   const fetchUser = async () => {
-//     try {
-//       setIsLoading(true);
-//       setError("");
-//       //  Get the current logged in user
-//       const {
-//         data: { user: authUser },
-//         error: authError,
-//       } = await supabase.auth.getUser();
-
-//       if (authError || !authUser) setError("No authenticated user found!");
-
-//       //  Get the user profile from the users table
-//       const { data: userData, error: userTableError } = await supabase
-//         .from("users")
-//         .select("*")
-//         .eq("user_id", authUser?.id)
-//         .single();
-
-//       if (!userData || userTableError) setError("Error fetching user profile");
-
-//       console.log(userData);
-//       //  Update the user in the redux store
-//       setIsLoading(false);
-//       return { success: true };
-//     } catch (err: any) {
-//       setIsLoading(false);
-//       setError("Error fetching user profile");
-//       return {success: false};
-//     }
-//   };
-
-//   return { fetchUser, isLoading, error };
-// };
